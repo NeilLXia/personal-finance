6e92e8dbba2efd4252e538c26ad1366eee094e17
Neil Xia <neil.l.xia@gmail.com>
2026-09-12 02:25:08 -0700

    update deploy script

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+
 set -e
 
+chown -R personal-finance:personal-finance /opt/personal-finance
+
 sudo -u personal-finance env HOME=/home/personal-finance bash -c '
+
   export NVM_DIR="$HOME/.nvm"
+
   source "$NVM_DIR/nvm.sh"
 
   cd /opt/personal-finance/node
+
   npm ci --omit=dev
+
 '
 
 /usr/bin/systemctl restart personal-finance
+
 /usr/bin/systemctl is-active --quiet personal-finance
